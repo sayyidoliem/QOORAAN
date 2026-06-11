bd261dcbb8f6992134f8fc23d3f6e55e114146d5
Olim <sayyidsunarko@gmail.com>
2026-06-11 14:56:38 +0700

    fix: update configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,18 @@
+import com.android.build.api.dsl.ApplicationExtension
+import org.gradle.kotlin.dsl.configure
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.android.built.kotlin)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.parcelize)//parcelable
-    alias(libs.plugins.ksp)//new method of kapt
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.ksp)
 }
 
-android {
+configure<ApplicationExtension> {
     namespace = "com.olimhousestudio.qooraan"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.olimhousestudio.qooraan"
@@ -23,19 +27,19 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+
     buildFeatures {
         compose = true
         buildConfig = true
@@ -43,59 +47,73 @@ android {
 }
 
 dependencies {
-
+    // AndroidX core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.activity.compose)
+
+    // Compose
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.room.common.jvm)
+    implementation(libs.androidx.material.icons.extended)
+
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Navigation
+    implementation(libs.androidx.navigation.compose)
+
+    // Coroutine
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Room
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+
+    // DataStore
+    implementation(libs.androidx.datastore.preferences)
+
+    // Permission
+    implementation(libs.accompanist.permissions)
+
+    // Google Play Services
     implementation(libs.play.services.location)
+
+    // Hilt compiler for WorkManager / AndroidX Hilt integration
+    ksp(libs.androidx.hilt.compiler)
+
+    // WebView
+    implementation(libs.agentweb.core)
+
+    // Network
+    implementation(libs.retrofit2.retrofit)
+    implementation(libs.converter.gson)
+
+    // SharedPreferences helper
+    implementation(libs.chibatching.kotpref)
+    implementation(libs.kotpref.initializer)
+    implementation(libs.enum.support)
+
+    // Media player
+    implementation(libs.player)
+
+    // Animation
+    implementation(libs.lottie.compose)
+
+    // Async image
+    implementation(libs.coil3.coil.compose)
+
+    // Unit test
     testImplementation(libs.junit)
+
+    // Android test
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-
-    //kotlin coroutine
-    implementation(libs.kotlinx.coroutines.android)
-    //navigation
-    implementation(libs.androidx.navigation.compose)
-    //room core
-    implementation(libs.androidx.room.runtime)
-    //room with kotlin Symbol Processing (KSP)
-    ksp(libs.androidx.room.compiler)
-    //Kotlin Extensions and Coroutines support for Room
-    implementation(libs.androidx.room.ktx)
-    //google permission
-    implementation(libs.accompanist.permissions)
-    //google service location
-    implementation(libs.play.services.location)
-    //hilt with work manager when using Kotlin.
-    ksp(libs.androidx.hilt.compiler)
-    //render web view in android
-    implementation(libs.agentweb.core)
-    //retrofit for network requests
-    implementation(libs.retrofit2.retrofit)
-    //retrofit with converter gson
-    implementation(libs.converter.gson)
-    //sharepreferenced core
-    implementation(libs.chibatching.kotpref)
-    //kotpref auto initialization module
-    implementation(libs.kotpref.initializer)
-    //kotpref support saving enum value and ordinal
-    implementation(libs.enum.support)
-    //media player android
-    implementation(libs.player)
-    //m3 material icon
-    implementation(libs.androidx.material.icons.extended)
-    //lottie compose animation
-    implementation(libs.lottie.compose)
-    //for async image
-    implementation(libs.coil3.coil.compose)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.android.built.kotlin) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.ksp) apply false//Kotlin Symbols Processing(KSP)
